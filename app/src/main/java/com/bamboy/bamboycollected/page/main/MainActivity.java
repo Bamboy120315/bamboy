77f676207351e1d9f98635f054ff652dc83331ad
bamboy <bamboy@vip.163.com>
2017-03-24 19:00:44 +0800

    主页开始

--- app/src/main/java/com/bamboy/bamboycollected/page/main/MainActivity.java
+++ app/src/main/java/com/bamboy/bamboycollected/page/main/MainActivity.java
@@ -1,23 +1,31 @@
 package com.bamboy.bamboycollected.page.main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.bamboy.bamboycollected.R;
 import com.bamboy.bamboycollected.base.BamboyActivity;
 
 public class MainActivity extends BamboyActivity {
 
+    private RelativeLayout rl_title;
+    private ImageView iv_back;
+    private TextView tv_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 关闭当前界面的右滑关闭功能
-        openSlideFinish(false);
     }
 
     @Override
     protected void findView() {
+        rl_title = (RelativeLayout) findViewById(R.id.rl_title);
+        iv_back = (ImageView) findViewById(R.id.iv_back);
+        tv_title = (TextView) findViewById(R.id.tv_title);
     }
 
     @Override
@@ -26,5 +34,14 @@ public class MainActivity extends BamboyActivity {
 
     @Override
     protected void init() {
+        // 关闭当前界面的右滑关闭功能
+        openSlideFinish(false);
+
+        // 设置titleBar
+        setImmerseTitleBar(rl_title);
+
+        iv_back.setVisibility(View.GONE);
+        tv_title.setText("主页");
+
     }
 }
