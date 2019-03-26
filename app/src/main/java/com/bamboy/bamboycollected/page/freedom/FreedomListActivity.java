5e9d5521eacedad4b417e84dd24ad488f56f2e00
bamboy <bamboy@vip.163.com>
2019-03-26 17:03:48 +0800

    非约束列表优化

--- app/src/main/java/com/bamboy/bamboycollected/page/freedom/FreedomListActivity.java
+++ app/src/main/java/com/bamboy/bamboycollected/page/freedom/FreedomListActivity.java
@@ -143,5 +143,23 @@ public class FreedomListActivity extends BamActivity implements FreedomCallback 
         mList.add(new BeanDialogueLeft("喵喵", "艾维巴蒂嗨起来！"));
         mList.add(new BeanDialogueRight("六六", "铲屎的怎么还不回来，是不是外边有狗了？"));
         mList.add(new BeanDialogueRight("六六", "我要吃肉肉…"));
+        mList.add(new BeanNewsImg(R.drawable.picture_b, "这些水果狗狗不能吃，你知道吗？"));
+        mList.add(new BeanMusic("成都", "赵雷 - 成都"));
+        mList.add(new BeanMusic("成全", "林宥嘉 - 翻唱合集"));
+        mList.add(new BeanMusic("单身情歌", "林志炫 - 一人一首成名曲"));
+        mList.add(new BeanMusic("浮夸", "林志炫 - 我是歌手"));
+        mList.add(new BeanMusic("改变自己", "王力宏 - 改变自己"));
+        mList.add(new BeanNewsText("微软2017年Build大会: 无Win10更新 AI成压轴戏", "北京时间5月11日凌晨消息，微软2017年Build开发者大会于美国西雅图当地时间上午8点拉开帷幕。 此次大会不仅吸引了全球众多开发者前来交流……"));
+        mList.add(new BeanDialogueRight("六六", "今晚单身狗吃狗粮"));
+        mList.add(new BeanDialogueLeft("喵喵", "狗粮不好吃，我要吃猫粮"));
+        mList.add(new BeanNewsText("微信“附近的小程序”悄然上线了，反观小程序为微信带来了什么", "“附近的小程序”正式开放：\n有小程序的商户，可以快速将门店小程序或普通小程序展示在“附近”。当用户走到某个地点，打开“发现-小程序-附近的小程序”，就能将自己附近的小程序“收入囊中”。"));
+        mList.add(new BeanNewsText("手上长水泡是什么引起的", "一到夏天，很多人手上都会出现一些小水泡，它们有的是以透明的形式存在，有的则是以半透明的方式存在，而且伴随着不同程度的瘙痒……"));
+        mList.add(new BeanMusic("骨子里的我", "李代沫 - 敏感者"));
+        mList.add(new BeanMusic("海角七号", "东来东往 - 路过·爱"));
+        mList.add(new BeanNewsImg(R.drawable.picture_b, "这些水果狗狗不能吃，你知道吗？"));
+        mList.add(new BeanDialogueLeft("喵喵", "铲屎的走了"));
+        mList.add(new BeanDialogueLeft("喵喵", "艾维巴蒂嗨起来！"));
+        mList.add(new BeanDialogueRight("六六", "铲屎的怎么还不回来，是不是外边有狗了？"));
+        mList.add(new BeanDialogueRight("六六", "我要吃肉肉…"));
     }
 }
